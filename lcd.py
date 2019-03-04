617c0d54fdb0bafd4038bd9127a5abedab1fadab
Martin Adonis Lopez Mendoza <43177741+martin11106@users.noreply.github.com>
2019-03-04 15:30:43 -0600

    flask

--- lcd.py
+++ lcd.py
@@ -1,6 +1,14 @@
 import lcddriver
 from time import *
-
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 lcd = lcddriver.lcd()
-
 lcd.lcd_display_string("Bitajor.com", 1)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+    socketio.run(app)
+
+
