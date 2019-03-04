f3863a873473abb7a2ef803cc788c88c9a841cbf
martin11106 <173230@ids.upchiapas.edu.mx>
2019-03-04 22:19:43 +0000

    actualizar

---
+++ main.py
@@ -0,0 +1,19 @@
+import lcddriver
+from time import *
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+lcd = lcddriver.lcd()
+lcd.lcd_display_string("Bitajor.com", 1)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app)
+@app.route('/')
+def index():
+	return "hello"
+@socketio.on('message')
+def handMessage(msg):
+	print('menssage'+msg)
+	send(msg,broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app)
